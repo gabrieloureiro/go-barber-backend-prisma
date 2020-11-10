60cc90851da3867803357dc739af0414667ba459
Gabriel Loureiro <gabrieloureirof@gmail.com>
2020-11-10 00:18:05 -0300

    Stop before validate date

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,9 @@ import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
 app.use(routes);
-app.use(express.json);
 
 app.listen(3333, () => {
-  console.log('AAA');
+  console.log('Server started on port 3333!!!');
 });
